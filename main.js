55fa8d5217c87c300bd877de7bafc0171d340255
Santiago Mendoza <santi48a@gmail.com>
2022-08-25 18:33:58 -0500

    agregando interacción a product detail y arreglando problemas de superposición

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon= document.querySelector('.product-detail-close')
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer=document.querySelector('#shoppingCartContainer');
 const cardsContainer=document.querySelector('.cards-container');
+const productDetailContainer=document.querySelector('#productDetail');
+
 
 
 
@@ -13,6 +16,8 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+productDetailCloseIcon.addEventListener('click',closeProductDetailAside)
 
 function toggleDesktopMenu(){
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -28,6 +33,8 @@ function toggleMobileMenu(){
     if (!isAsideClosed){
       shoppingCartContainer.classList.add('inactive');
     }
+
+    closeProductDetailAside();
     mobileMenu.classList.toggle('inactive');
     }
 
@@ -37,10 +44,26 @@ function toggleMobileMenu(){
     if (!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
     }
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    
+    if (!isProductDetailClosed){
+      productDetailContainer.classList.add('inactive');
+    }
     
     shoppingCartContainer.classList.toggle('inactive')
 
   }
+
+ function openProductDetailAside(){
+
+  shoppingCartContainer.classList.add('inactive')  ;
+
+  productDetailContainer.classList.remove('inactive');
+ }
+
+ function closeProductDetailAside(){
+  productDetailContainer.classList.add('inactive');
+ }
 
   const productList=[];
   
@@ -60,18 +83,7 @@ function toggleMobileMenu(){
     img:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
   });
 
-//   <div class="product-card">
-//   <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-//   <div class="product-info">
-//     <div>
-//       <p>$120,00</p>
-//       <p>Bike</p>
-//     </div>
-//     <figure>
-//       <img src="./icons/bt_add_to_cart.svg" alt="">
-//     </figure>
-//   </div>
-// </div>
+
 
   
 
@@ -82,6 +94,7 @@ function toggleMobileMenu(){
     
         const productImg= document.createElement('img');
         productImg.setAttribute('src', product.img);
+        productImg.addEventListener('click', openProductDetailAside)
     
         const productInfo =document.createElement('div');
         productInfo.classList.add('product-info');
